fe00fb116005c199d5bb2019f497f1d3eac934f6
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-01-15 11:20:48 +0100

    implement edit invoice functionality with update and delete actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,6 +12,7 @@ const FormSchema = z.object({
     date: z.string(),
 });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -29,4 +30,31 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    VALUES (${customerId}, ${amountInCents}, ${status})
+    WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
 }
